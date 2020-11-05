9d5cafdd746af560d97891ffac2dd18e510e8f05
Kieran Smith <kieransmith63@gmail.com>
2020-11-05 14:45:51 +0000

    Added posts to database
    Database now holds all current posts which now display on my blog home page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Kieran Smith',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'datePosted': 'November 4th 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'datePosted': 'November 5th 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
 
     return render(request, 'blog/home.html', context)
